c19c311bcb380bf221bc1db75c7d434749dc8120
patrickcmd <pwalukagga@gmail.com>
2019-11-21 14:36:02 +0300

    update users list component

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 import axios from 'axios';
 
-import AddUser from './components/AddUser';
 import About from "./components/About";
 import Form from "./components/Form";
 import NavBar from "./components/NavBar";
@@ -117,18 +116,7 @@ class App extends Component {
                                 <br/>
                                 <Switch>
                                     <Route exact path="/" render={() => (
-                                        <div>
-                                            <h1 className="title is-1 is-1">All Users</h1>
-                                            <hr/><br/>
-                                            <AddUser
-                                                username={this.state.username}
-                                                email={this.state.email}
-                                                addUser={this.addUser}
-                                                handleChange={this.handleChange}
-                                            />
-                                            <br/><br/>
-                                            <UsersList users={this.state.users}/>
-                                        </div>
+                                        <UsersList users={this.state.users}/>
                                     )} />
                                     <Route exact path="/about" component={About}/>
                                     <Route exact path="/register" render={()=> (
